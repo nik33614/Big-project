92ad400467f4e5affb73c33d64f7d9e8506fe85e
Nikita <58114134+nik33614@users.noreply.github.com>
2019-12-27 18:06:24 +0300

    Add files via upload

---
+++ Enemyboat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemyboat : MonoBehaviour
+{
+
+    public float Speed = 2;
+    
+    Vector3 target;
+
+
+    
+    void Update()
+    {
+        //searching for player
+        target = GameObject.FindWithTag("Player").transform.position;
+        // go to target
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * Speed);
+    }
+}
